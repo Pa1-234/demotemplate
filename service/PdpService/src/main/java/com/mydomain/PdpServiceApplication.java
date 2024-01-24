45002736ae548cb8d7226e26db4021af42fa69cf
nikshithas291993 <nikshitha.s@sonata-software.com>
2024-01-24 11:51:52 +0530

    Initial Commit

---
+++ service/PdpService/src/main/java/com/mydomain/PdpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mydomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdpServiceApplication.class, args);
+	}
+
+}
